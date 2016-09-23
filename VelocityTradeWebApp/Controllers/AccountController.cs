76da4af68d661803909c2117cf7134de2b262c53
AdamsMichael <michaeladams235@gmail.com>
2016-09-23 13:51:53 +0200

    Redirect Update

--- VelocityTradeWebApp/Controllers/AccountController.cs
+++ VelocityTradeWebApp/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace VelocityTradeWebApp.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Search", "Home", new { Auth = result });
                 }
                 AddErrors(result);
             }
